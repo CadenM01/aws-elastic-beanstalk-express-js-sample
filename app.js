c72e576f3a252094870e56e13cc3867ebcb4f650
CadeM01 <cadenmaki1@gmail.com>
2025-11-20 23:12:56 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Goodbye World!'));
+app.get('/', (req, res) => res.send('I successfully finished the tutorial!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
